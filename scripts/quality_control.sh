594b035a0144b0094f737a126e5b64f6bc7b3ff7
Korbinian Weisensee <korbinian.weisensee@gmail.com>
2024-11-09 10:09:13 +0100

    troubleshooting for the FastQC script

--- scripts/quality_control.sh
+++ scripts/quality_control.sh
@@ -4,17 +4,28 @@
 #SBATCH --mem=1g
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=quality_control
-#SBATCH --output=../logfiles/array_%J.out   # Standard output
-#SBATCH --error=../logfiles/array_%J.err    # Standard error
+#SBATCH --output=../logfiles/QC_%J.out   # Standard output
+#SBATCH --error=../logfiles/QC_%J.err    # Standard error
 #SBATCH --partition=pibu_el8
 
-# define variables
+
+# Load FastQC module from specified path
+#module add UHTS/Quality_control/fastqc/0.12.1
+
+FASTQC_IMAGE="/containers/apptainer/fastqc-0.12.1.sif"
+
+# Define variables
 WORKDIR="/data/users/kweisensee/RNA_Seq"
-OUTDIR="$WORKDIR/output"
+OUTDIR="${WORKDIR}/logfiles"
 SAMPLELIST="$WORKDIR/output/samplelist.tsv"
 
-SAMPLE=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $1; exit}' $SAMPLELIST`
-READ1=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $2; exit}' $SAMPLELIST`
-READ2=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $3; exit}' $SAMPLELIST`
+# Extract sample information
+SAMPLE=$(awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $1; exit}' $SAMPLELIST)
+READ1=$(awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $2; exit}' $SAMPLELIST)
+READ2=$(awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $3; exit}' $SAMPLELIST)
 
-OUTFILE="$OUTDIR/${SAMPLE}.txt"
+# Run FastQC on the sample's reads
+#fastqc -o "$OUTDIR" -f fastq "$READ1" "$READ2"
+
+# Run FastQC inside Singularity
+singularity exec "$FASTQC_IMAGE" fastqc -o "$OUTDIR" -f fastq "$READ1" "$READ2"
