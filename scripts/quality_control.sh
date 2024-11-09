1a9e115b9a4c18dd04fdb217fbd29a58fc5a7f5b
Korbinian Weisensee <korbinian.weisensee@gmail.com>
2024-11-09 22:04:18 +0100

    further testing for the QC script

--- scripts/quality_control.sh
+++ scripts/quality_control.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --array=1-12
-#SBATCH --time=00:10:00
+#SBATCH --time=00:30:00
 #SBATCH --mem=1g
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=quality_control
@@ -8,15 +8,16 @@
 #SBATCH --error=../logfiles/QC_%J.err    # Standard error
 #SBATCH --partition=pibu_el8
 
-
 # Load FastQC module from specified path
 #module add UHTS/Quality_control/fastqc/0.12.1
 
-FASTQC_IMAGE="/containers/apptainer/fastqc-0.12.1.sif"
+module load FastQC/0.11.9-Java-11
+
+#FASTQC_IMAGE="/containers/apptainer/fastqc-0.12.1.sif"
 
 # Define variables
 WORKDIR="/data/users/kweisensee/RNA_Seq"
-OUTDIR="${WORKDIR}/logfiles"
+OUTDIR="${WORKDIR}/output/fastqc_1"
 SAMPLELIST="$WORKDIR/output/samplelist.tsv"
 
 # Extract sample information
@@ -28,4 +29,7 @@ READ2=$(awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $3; exit}' $SAMPLELIST)
 #fastqc -o "$OUTDIR" -f fastq "$READ1" "$READ2"
 
 # Run FastQC inside Singularity
-singularity exec "$FASTQC_IMAGE" fastqc -o "$OUTDIR" -f fastq "$READ1" "$READ2"
+# setting -e and -B is not working either
+#singularity exec "$FASTQC_IMAGE" fastqc -o "$OUTDIR" -f fastq "$READ1" "$READ2"
+
+fastqc -o "$OUTDIR" -f fastq "${WORKDIR}/rawdata/${READ1}" "${WORKDIR}/rawdata/${READ2}"
