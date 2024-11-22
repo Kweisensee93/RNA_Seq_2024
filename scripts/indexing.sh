7eaae4989b209d4ab6ef3467bd2f08a7f900219b
kweisensee <korbinian.weisensee@gmail.com>
2024-11-22 16:45:41 +0100

    Created the indexing.sh to index the reference genome. created output/indexing folder

---
+++ scripts/indexing.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --time=04:00:00
+#SBATCH --mem=8g
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=indexing
+#SBATCH --output=../logfiles/indexing_%J.out   # Standard output
+#SBATCH --error=../logfiles/indexing_%J.err    # Standard error
+#SBATCH --partition=pibu_el8
+
+HISAT2_IMAGE="/containers/apptainer/hisat2_samtools_408dfd02f175cd88.sif"
+
+WORKDIR="/data/users/kweisensee/RNA_Seq"
+OUTDIR="${WORKDIR}/output/indexing"
+
+# Unzip reference genome for further processing, if not already present
+if [ ! -f ${WORKDIR}/reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa ]; then
+    gunzip -c ${WORKDIR}/reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz > \
+        ${WORKDIR}/reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+fi
+
+apptainer exec --bind /data ${HISAT2_IMAGE} hisat-2-build \
+    -p 16 \
+    ${WORKDIR}/reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa genome \
+    > ${OUTDIR}/indexing.log 2>&1
