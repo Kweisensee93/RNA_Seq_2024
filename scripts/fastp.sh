555be4b70d2aa0e3b7411295155dd0c3bf35b778
kweisensee <korbinian.weisensee@gmail.com>
2024-11-22 10:20:50 +0100

    renaming of fastp and fastqc_2 files to remove the double _. Altered the gitignore files. Removed all and created a new one in the main repository that ignores all but the scripts

--- scripts/fastp.sh
+++ scripts/fastp.sh
@@ -20,13 +20,13 @@ READ2=$(awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $3; exit}' $SAMPLELIST)
 
 #for the analysis fastp version 0.23.2 was used
 FASTP_IMAGE="/containers/apptainer/fastp_0.23.2--h5f740d0_3.sif"
-#load the fastp module
-#bind your specific folder for rerun
+# load the fastp module
+# bind your specific folder for rerun
 apptainer exec --bind /data ${FASTP_IMAGE} fastp \
     --dont_overwrite \
     -h "${OUTDIR}/${SAMPLE}_fastp.html" \
     --detect_adapter_for_pe \
     -i "${WORKDIR}/rawdata/${READ1}" \
     -I "${WORKDIR}/rawdata/${READ2}" \
-    -o "${OUTDIR}/${SAMPLE}_fastp__R1.fastq.gz" \
-    -O "${OUTDIR}/${SAMPLE}_fastp__R2.fastq.gz"
+    -o "${OUTDIR}/${SAMPLE}_fastp_R1.fastq.gz" \
+    -O "${OUTDIR}/${SAMPLE}_fastp_R2.fastq.gz"
